9e859e011f6de168d7d4559171a81224fe70bde0
leejaehun <ldyjka@gmail.com>
2023-11-02 22:56:31 +0900

    Initial commit

---
+++ src/test/java/ADM/AllergyMapData/AllergyMapDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ADM.AllergyMapData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllergyMapDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
